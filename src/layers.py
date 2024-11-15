47654f0466fd0e2287b5e2b4fcf216db60c200fe
Ethan Harvey <ethan.harvey@tufts.edu>
2024-11-15 12:29:20 -0500

    Added outputscale

--- src/layers.py
+++ src/layers.py
@@ -2,6 +2,28 @@ import math
 # PyTorch
 import torch
 
+class RandomFeatureGaussianProcess(torch.nn.Module):
+    def __init__(self, in_features, out_features, lengthscale=math.sqrt(20), outputscale=1.0, rank=1024):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.lengthscale = lengthscale
+        self.outputscale = outputscale
+        self.rank = rank
+        self.register_buffer('feature_weight', torch.randn(self.rank, self.in_features))
+        self.register_buffer('feature_bias', 2 * torch.pi * torch.rand(self.rank))
+        self.linear = torch.nn.Linear(in_features=self.rank, out_features=self.out_features, bias=False)
+                                        
+    def featurize(self, h):
+        features = torch.nn.functional.linear(h, -(1/self.lengthscale**2) * self.feature_weight, self.feature_bias)
+        return (2 / self.rank) ** 0.5 * torch.cos(features)
+        
+    def forward(self, h, return_variance=True):
+        batch_size, hidden_dim = h.shape
+        features = self.outputscale*self.featurize(h)
+        logits = self.linear(features)
+        return logits
+
 class VariationalLinear(torch.nn.Module):
     def __init__(self, layer, sigma_param, use_posterior=False):
         super().__init__()
@@ -26,24 +48,3 @@ class VariationalLinear(torch.nn.Module):
     @property
     def variational_bias(self):
         return self.layer.bias + torch.nn.functional.softplus(self.sigma_param) * torch.randn_like(self.layer.bias).to(self.layer.bias.device) if self.layer.bias is not None else None
-
-class RandomFeatureGaussianProcess(torch.nn.Module):
-    def __init__(self, in_features, out_features, lengthscale=math.sqrt(20), rank=1024):
-        super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.lengthscale = lengthscale
-        self.rank = rank
-        self.register_buffer('feature_weight', torch.randn(self.rank, self.in_features))
-        self.register_buffer('feature_bias', 2 * torch.pi * torch.rand(self.rank))
-        self.linear = torch.nn.Linear(in_features=self.rank, out_features=self.out_features, bias=False)
-                                        
-    def featurize(self, h):
-        features = torch.nn.functional.linear(h, -(1/self.lengthscale**2) * self.feature_weight, self.feature_bias)
-        return (2 / self.rank) ** 0.5 * torch.cos(features)
-        
-    def forward(self, h, return_variance=True):
-        batch_size, hidden_dim = h.shape
-        features = self.featurize(h)
-        logits = self.linear(features)
-        return logits
